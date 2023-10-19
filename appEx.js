ec3334649e21d0b1ed47ab3ff72b514a89a7e200
EduardoTrinidad <141768717+EduardoTrinidad@users.noreply.github.com>
2023-10-19 09:38:30 -0600

    primercommitt

---
+++ appEx.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port=8080;
+//Servir un contenido estatico
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.send('Home page')
+});
+
+app.get('/holamundo', function (req, res) {
+    res.send('hola mundo')
+  })
+
+app.get('*', (req, res)=> {
+    res.sendFile(__dirname+'/public/404.html')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
